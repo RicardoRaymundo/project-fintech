a4096717fb7e91b967585c8bba3c111f6a0e76d4
macbookpro <1>
2019-08-03 10:30:17 -0300

    Alteração do app.template

--- src/@modules/fintech/currency/currency.module.ts
+++ src/@modules/fintech/currency/currency.module.ts
@@ -15,3 +15,7 @@ import {CurrencyRouting} from './currency.routing';
   ]
 })
 export class CurrencyModule { }
+
+// stackblitz.com/github/RicardoRaymundo/project-fintech
+// https://github.com/RicardoRaymundo/project-fintech/tree/0989d3770cea2a46dcf5f6f6f26f34077b8b80b1
+// https://github.com/RicardoRaymundo/project-fintech
