6b072b8db34b54ec5127be28be97ba7b526941a2
agoeiro <iagoeiro@hotmail.com>
2019-08-05 19:33:39 -0300

    Problemas com parâmetros

--- src/@modules/currency/list/currency-list.component.ts
+++ src/@modules/currency/list/currency-list.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {TranslateConfig} from '../../../@plugins/config/translate.config';
 import {ActivatedRoute} from '@angular/router';
 
 
@@ -9,10 +11,12 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class CurrencyListComponent implements OnInit {
 
-  // constructor(@Inject(ActivatedRoute) private _activatedRoute: ActivatedRoute) {
-  constructor(private _activatedRoute: ActivatedRoute) {
+  constructor(public translateService: TranslateService,
+              public translateConfig: TranslateConfig,
+              private _activatedRoute: ActivatedRoute) {
+
     // Implementa internacionalização
-    // this._configTranslate();
+    this._configTranslate();
 
     this._activatedRoute.params.subscribe((params: any) => {
       console.log('PARAMS', params);
@@ -27,11 +31,11 @@ export class CurrencyListComponent implements OnInit {
    */
   private _configTranslate(): void {
     const REGEX: any = /en|pt/;
-    /*this.translateService.addLangs(['en', 'pt']);
+    this.translateService.addLangs(['en', 'pt']);
     this.translateService.setDefaultLang(this.translateConfig.defaultLanguage);
     this.translateService.use(this.translateConfig.activeLang(REGEX));
     this.translateConfig.changeLang.subscribe(() => {
       this.translateService.use(this.translateConfig.activeLang(REGEX));
-    });*/
+    });
   }
 }
