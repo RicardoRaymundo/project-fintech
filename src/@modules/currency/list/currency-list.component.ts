18756e890d36d01888719d9fc30a8b0b1bb6febd
agoeiro <iagoeiro@hotmail.com>
2019-08-07 07:14:49 -0300

    Implementação da tabela em Moedas seleção de item

--- src/@modules/currency/list/currency-list.component.ts
+++ src/@modules/currency/list/currency-list.component.ts
@@ -1,30 +1,31 @@
-import {AfterViewInit, Component, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Component, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {TranslateConfig} from '../../../@plugins/config/translate.config';
 import {ActivatedRoute} from '@angular/router';
 import {MatPaginator, MatSort} from '@angular/material';
-import {HttpClient} from '@angular/common/http';
-import {merge, Observable, of as observableOf} from 'rxjs';
-import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {exist} from '@use-pattern/utils';
+
 
 export interface PeriodicElement {
+  _id: string;
   name: string;
   position: number;
   weight: number;
   symbol: string;
 }
 
+
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {_id: '1', position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {_id: '2', position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {_id: '3', position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {_id: '4', position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {_id: '5', position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {_id: '6', position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {_id: '7', position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {_id: '8', position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {_id: '9', position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {_id: '10', position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
 
@@ -34,10 +35,14 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./currency-list.styles.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class CurrencyListComponent  {
+export class CurrencyListComponent implements OnInit {
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) public sort: MatSort;
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'star'];
   dataSource = ELEMENT_DATA;
+  public selectedIndex: string | any;
+  public selectedItem: any;
 
   constructor(@Inject(TranslateService) public translateService: TranslateService,
               @Inject(TranslateConfig) public translateConfig: TranslateConfig,
@@ -47,6 +52,19 @@ export class CurrencyListComponent  {
     this._configTranslate();
   }
 
+  public ngOnInit(): void {
+    this.paginator._intl.itemsPerPageLabel = 'Items por página';
+  }
+
+  public onHighlight(item: any): void {
+    // Verifica se o item possui a propiedade id
+    if (item.hasOwnProperty('_id') && exist(item['_id'])) {
+      // Registra o id do item selecionado
+      this.selectedIndex = item['_id'];
+      // Registra o item selecionado
+      this.selectedItem = item;
+    }
+  }
 
   /**
    * Configura componentes de internacionalização
