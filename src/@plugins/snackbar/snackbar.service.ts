e518064d9d67faa6f33e3ecb8cd20ad0ecfe4274
agoeiro <iagoeiro@hotmail.com>
2019-08-10 14:44:55 -0300

    Implementado módulo de Autorização

--- src/@plugins/snackbar/snackbar.service.ts
+++ src/@plugins/snackbar/snackbar.service.ts
@@ -1,19 +1,21 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {MatSnackBar} from '@angular/material';
 import {SnackbarViewComponent} from './view/snackbar-view.component';
 import {SnackbarViewInterface} from './view/snackbar-view.interface';
 
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class SnackbarService {
 
-  constructor(private snackBar: MatSnackBar) {
+  constructor(@Inject(MatSnackBar) private snackBar: MatSnackBar) {
   }
 
   public open(data: SnackbarViewInterface): void {
     this.snackBar.openFromComponent(SnackbarViewComponent, {
       duration: 2500,
-      data: data
+      'data': data
     });
   }
 }
